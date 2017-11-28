128c23938103cbc0a605b7970c35519224e6cd67
MaximoB <maximo.berocay@gmail.com>
2017-11-28 13:21:19 -0600

    Add faster inline version of ToDateTimeOffset func

---
+++ Database/Functions/ToDateTimeOffset_Inline.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [Tzdb].[ToDateTimeOffset_Inline]
+(
+    @time DATETIME2,
+    @tz VARCHAR(50)
+)
+RETURNS TABLE
+WITH SCHEMABINDING
+AS RETURN
+  SELECT TOP 1
+         TODATETIMEOFFSET(@time, ntrvl.[OffsetMinutes])
+         AS [Time]
+  FROM   [Tzdb].[Intervals] ntrvl
+  WHERE  ntrvl.[ZoneId] = (SELECT zn.[ZoneId] FROM [Tzdb].[GetZoneId_Inline](@tz) zn)
+  AND    ntrvl.[LocalStart] <= @time
+  AND    ntrvl.[LocalEnd] > @time;
+GO
